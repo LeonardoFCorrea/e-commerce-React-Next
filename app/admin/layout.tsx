150146329a73ec2a7cd41f0ea58e62fa04514b68
LeonardoFCorrea <leonardofelixcorrea@gmail.com>
2026-01-28 16:07:09 -0300

    Create Models | Admin Dashboard WIP

---
+++ app/admin/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Nav, NavLink } from "../components/nav.component";
+
+export default function AdminLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return <>
+    <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+    </Nav>
+    <div className="container my-6">{children}</div>    
+    </>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={cn("bg-background min-h-screen font-sans antialiased", inter.variable)}>{children}</body>
     </html>
   );
 }
